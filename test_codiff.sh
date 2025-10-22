efb981b8cebeda4f78675f3a0ac7bb1e6c6324bf
Ceaby Fernandez <ceabyfernandez@dsl-trebng22-54f937-142.dhcp.inet.fi>
2025-10-22 05:17:48 +0300

    codiff changes

--- test_codiff.sh
+++ test_codiff.sh
@@ -1,11 +1,11 @@
 export CUDA_VISIBLE_DEVICES=5
 export HF_ENDPOINT=https://hf-mirror.com
 
-dataset=DIV2K_valid
+dataset=Happywhale
 
 python main_test_codiff.py \
-  -i /data/dataset/CAR/${dataset} \
+  -i /Users/ceabyfernandez/bachelorsthesis/Datasets/Happywhale/testimages_mini \
   -o test_results/$dataset \
-  --pretrained_model /data/pretrained/stable-diffusion-2-1-base \
-  --cave_path model_zoo/cave.pth \
-  --codiff_path model_zoo/codiff.pkl
+  --pretrained_model /Users/ceabyfernandez/bachelorsthesis/CODiff/model_zoo/v2-1_512-ema-pruned.ckpt\
+  --cave_path /Users/ceabyfernandez/bachelorsthesis/CODiff/model_zoo/cave.pth \
+  --codiff_path /Users/ceabyfernandez/bachelorsthesis/CODiff/model_zoo/codiff.pkl
